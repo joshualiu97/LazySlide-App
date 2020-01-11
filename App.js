f527af251b23b6a118e9b65dff12e7736afe654e
Joshua Liu <joshua159230@gmail.com>
2020-01-11 12:02:23 -0800

    Home.js file

--- App.js
+++ App.js
@@ -9,12 +9,7 @@ import {
 } from 'react-native';
 import { AuthSession } from 'expo';
 import * as Font from 'expo-font';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-// });
-const instructions = "Open and close your curtains with only a press of a button!";
+import Home from './components/Home.js'
 
 export default class App extends Component {
   componentDidMount() {
@@ -26,92 +21,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-      {/* flex: 1, main container  */}
-        
-        <View style={styles.header}>
-        {/* top half, contains title and intro message */}
-          <Text style={styles.title}>LazySlide</Text>
-          <Text style={styles.instructions}>{instructions}</Text>
-        </View>
-
-        <View style={styles.button_container}>
-        {/* bottom half container */}
-          <View style={styles.buttons}>
-          {/* button container */}
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => Alert.alert('Opening curtains')}
-            >
-              <Text style={styles.button_text}>Open</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => Alert.alert('Closing curtains')}
-            >
-              <Text style={styles.button_text}>Close</Text>
-            </TouchableOpacity>
-          </View>
-
-        </View>
-      </View>
+      <Home/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    fontFamily: "raleway-regular",
-
-    flex: 1,
-    alignItems: 'center',
-    justifyContent:'center',
-    backgroundColor: 'lightblue',
-  },
-  
-  header: {
-    flex:2,
-    // backgroundColor: 'red',
-    width: '80%',
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  title: {
-    fontFamily: "raleway-bold",
-    fontSize: 60,
-    fontWeight: 'bold',
-  },
-  instructions: {
-    fontFamily: "raleway-regular",
-    fontSize: 20,
-    textAlign: 'center',
-  },
-
-  button_container: {
-    flex: 2,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  buttons: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    color: 'white',
-    // backgroundColor: 'red',
-  },
-  button: {
-    borderRadius: 10,
-    backgroundColor: '#627E8D',
-    width: 150,
-    height: 60,
-    margin: 10,
-    alignItems: 'center',
-    justifyContent:'center',
-  },
-  button_text: {
-    fontFamily: "raleway-regular",
-    color: 'white',
-    fontSize: 20,
-  },
-});
